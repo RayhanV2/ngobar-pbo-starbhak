bb8c0a0b819078257370896a69233d3beb168d40
mhaiiih <mhmmadairil@gmail.com>
2022-08-03 17:08:28 +0700

    test

--- resources/views/starter.blade.php
+++ resources/views/starter.blade.php
@@ -7,7 +7,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Starter</title>
+  <title>NGOBAR KUYY</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -90,7 +90,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
               <img src="{{ asset('dist/img/kato.jpg') }}" alt="User Avatar" class="img-size-50 img-circle mr-3">
               <div class="media-body">
                 <h3 class="dropdown-item-title">
-                  Megumi Kato
+                  burh
                   <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
                 </h3>
                 <p class="text-sm">Kawaaiiii</p>
@@ -163,10 +163,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    {{-- <a href="index3.html" class="brand-link">
+    <a href="index3.html" class="brand-link">
       <img src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">Uji Level</span>
-    </a> --}}
+      <span class="brand-text font-weight-light">NGOBAR KUYY</span>
+    </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
@@ -246,7 +246,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">{{ Auth::user()->level }}</li>
+              {{-- <li class="breadcrumb-item active">{{ Auth::user()->level }}</li> --}}
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
